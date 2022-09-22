d395e33dd004a678b81438a409483854b83c1b5a
Jaime Lucea <jaime.lucea@atactic.io>
2022-09-22 16:20:09 +0200

    Initial Commit

---
+++ BeerMonster/BeerMonsterApp.swift
@@ -0,0 +1,20 @@
+//
+//  BeerMonsterApp.swift
+//  BeerMonster
+//
+//  Created by Jaime Lucea on 22/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BeerMonsterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
